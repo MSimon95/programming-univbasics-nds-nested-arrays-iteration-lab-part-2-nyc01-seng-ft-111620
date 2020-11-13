3ad64069389be6cfd061f4280346487de31eb12b
madiwa Simon <mad.simon01@gmail.com>
2020-11-13 16:38:01 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,14 +8,11 @@ while row_index < src.count do
   longest_string_element = ""
   while element_index < spice_rack[row_index].count do
 
-    if src[row_index][element_index].length > longest_string_element.length
+    if src[row_index][element_index].min > longest_string_element.min
       longest_string_element = spice_rack[row_index][element_index]
     end
     element_index += 1
   end
- 
-  # We have to check every element in each inner array, so after the loop finishes
-  # The current value of longest_string_element is pushed into outer_results
   outer_results << longest_string_element
   row_index += 1
 end
