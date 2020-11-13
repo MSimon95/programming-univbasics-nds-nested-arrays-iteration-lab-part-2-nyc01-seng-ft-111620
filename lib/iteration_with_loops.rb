937ddcfb1f42b687544b107940c5313bf9eb4aa5
madiwa Simon <mad.simon01@gmail.com>
2020-11-13 16:50:36 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,10 +5,10 @@ outer_results = []
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  min_num = []
+  min_num = 0
   while element_index < src[row_index].count do
 
-    if src[row_index][element_index] < min_num
+    if src[row_index][element_index] > min_num
       min_num = src[row_index][element_index]
     end
     element_index += 1
